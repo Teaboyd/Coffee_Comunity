dcc6b498eacaa1b25927499c53cede7b7fb6dba3
Patiphan Phuengphop <159489011+Teaboyd@users.noreply.github.com>
2024-11-27 16:10:13 +0700

    fixed(token): fixed protects files for authentication system

--- middlewares/protect.mjs
+++ middlewares/protect.mjs
@@ -1,25 +1,31 @@
 import jwt from 'jsonwebtoken';
-
+import User from "../modules/users.mjs";
 
 export const protect = async (req,res,next) => {
         const token = req.headers.authorization
 
     if(!token || !token.startsWith('Bearer')){
-        res.status(401).json({
+        return res.status(401).json({
             message:"Token has invalid format"
         });
     }
 
     const tokenWithoutBearer = token.split(" ")[1];
 
-    jwt.verify(tokenWithoutBearer,process.env.SECRET_KEY,(err,payload) => {
-        if (err){
-            return res.status(401).json({
-                message: "Token is invalid"
-            });
-        }
+    try{
+    const decode = jwt.verify(tokenWithoutBearer,process.env.SECRET_KEY)
+    const user = await User.findById(decode.id);
+    if(!user){
+        return res.status(404).json({
+                message: "User not found"
+        });
+    };
 
-        req.user_id = payload.id;
+        req.user = user
         next();
-    });
+    }catch(err){
+        return res.status(401).json({
+                message: "Token is invalid"
+        });
+    };
 }   
