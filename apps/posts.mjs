1f21aed097e8900b3629d6e05aafa6546dc5ed7a
Teaboyd <ph.patiphan@kkumail.com>
2024-11-29 00:40:36 +0700

    feat(api): add CRUD api of posts

--- apps/posts.mjs
+++ apps/posts.mjs
@@ -59,10 +59,63 @@ postRouter.get("/" , [protect] , async (req,res) => {
     };
 });
 
+postRouter.put("/:postId" , [protect] , async (req,res) =>{
 
+    try{
+    const updatePost = new ObjectId(req.params.postId)
+    const { title , postBody , imageAndVdo , link } = req.body
 
+    const newPost = ({
+        users:req.user_id,
+        title,
+        postBody,
+        imageAndVdo,
+        link,
+        updated_at: new Date()
+    });
 
+    await Post.findByIdAndUpdate(
+        updatePost,
+        {$set: newPost},
+        {new: true}
+    );
 
+    return res.status(201).json({
+        message: "Post has been updated"
+    });
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({
+           message: "Post cannot updated because database issue", 
+        });
+    }
+});
 
+postRouter.delete("/:postId" , [protect] , async (req,res) => { 
+    
+    try{
+        const postId = req.params.postId
+        const postChecker = Post.findById({postId});
+
+        if ( !postChecker ){
+            return res.status(404).json({
+                message: "Post not found"
+            });
+        };
+
+        await Post.findByIdAndDelete(postId);
+
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({
+            message: "This post cannot be deleted because database issue"
+      });
+    };
+
+    return res.status(200).json({
+        message: "Post delete successfully"
+    });
+
+});
 
 export default postRouter;
