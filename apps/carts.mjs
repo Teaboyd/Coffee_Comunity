9fc741b9173fd426b7aa3903d9996a18ddaac454
Teaboyd <ph.patiphan@kkumail.com>
2024-12-05 02:12:02 +0700

    feat(api): createcartsProducts

--- apps/carts.mjs
+++ apps/carts.mjs
@@ -30,7 +30,33 @@ cartRouter.post("/" , [protect] , async (req,res) =>{
 	}
 });
 
-cartRouter.post("/cart" , [protect] , async (req,res) => {
+cartRouter.post("/carts" , [protect] , async (req,res) => {
+
+	try{
+	const {products , carts , quantity} = req.body
+	const  productCart = new CartProduct ({
+		userId : req.user_id,
+		products,
+		carts,
+		quantity,
+		created_at: new Date(),
+		updated_at: new Date(),
+	});
+
+	await productCart.save();
+
+	return res.status(201).json({
+		message: "Product has added"
+	});
+	}catch(err){
+		console.log(err)
+	   return res.status(500).json({
+		message: "Cart can't create because database issue",
+	   });
+	}	
+});
+
+cartRouter.get("/" , [protect] , async (req,res) => {
 
 });
 
